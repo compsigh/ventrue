a33eed5afe638583df83cf0f2fb01c6133e33765
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-02-19 02:15:41 -0800

    Nest views in between navbar and footer (layout)

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p>Made by compsigh</p>
+            <p>Made by compsigh in <strong>24 hours</strong></p>
             <p>With ❤️ from San Francisco, CA</p>
             <p><a>Contact us</a></p>
         </footer>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 export default function Navbar() {
     return (
     <nav>
         <div className="logo">
             <img src="/logo.png" alt="Ventrue logo" />
         </div>
-        <a>Home</a>
-        <a>Projects</a>
-        <a>Teammates</a>
+        <Link href="/">Home</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/teammates">Teammates</Link>
     </nav>
     );
 }
