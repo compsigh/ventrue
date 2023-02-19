a67fe51c15a5739dcef79e7ad0c1abc14fea799d
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-02-19 02:00:16 -0800

    Create placeholder Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+    <nav>
+        <div className="logo">
+            <img src="/logo.png" alt="Ventrue logo" />
+        </div>
+        <a>Home</a>
+        <a>Projects</a>
+        <a>Teammates</a>
+    </nav>
+    );
+}
