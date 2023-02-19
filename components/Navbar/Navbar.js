ec68b45069a19b09ab26863c800f869fc1c49463
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-02-19 03:27:33 -0800

    Add Navbar component styling

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from './Navbar.module.scss';
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-    <nav>
+    <nav className={styles.navbar}>
         <div className="logo">
-            <img src="/logo.png" alt="Ventrue logo" />
+            <Image src="/logo.png" alt="Vercel Logo" width={50} height={50} />
         </div>
         <Link href="/">Home</Link>
         <Link href="/projects">Projects</Link>
