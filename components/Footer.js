21851943db7f765834f0ff8137f140a35f862538
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-02-19 02:00:26 -0800

    Create placeholder Footer component

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Made by compsigh</p>
+            <p>With ❤️ from San Francisco, CA</p>
+            <p><a>Contact us</a></p>
+        </footer>
+    );
+}
